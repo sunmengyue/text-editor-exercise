ed3b63e16d57402c5699f5ed543216a7adeba7c5
Simon <bardrabbit@gmail.com>
2021-02-01 01:14:09 -0500

    add text-style buttons

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
+const input = document.querySelector('#text-input');
+const outPut = document.querySelector('#text-output');
+
 function updateText() {
-  let inputText = document.querySelector('#text-input').value;
-  let outPut = document.querySelector('#text-output');
+  let inputText = input.value;
   outPut.innerText = inputText;
+}
+
+// function makeBold(elem) {
+//   elem.classList.toggle('active');
+//   outPut.classList.toggle('bold');
+// }
+
+// function makeItalic(elem) {
+//   elem.classList.toggle('active');
+//   outPut.classList.toggle('italic');
+// }
+
+// function makeUnderline(elem) {
+//   elem.classList.toggle('active');
+//   outPut.classList.toggle('underline');
+// }
+
+function makeTexStyle(elem, style) {
+  elem.classList.toggle('active');
+  outPut.classList.toggle(style);
 }
