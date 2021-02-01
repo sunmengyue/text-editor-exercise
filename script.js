0e3c8703681c5e0bf2a24b9e831d66b9ffe2bea1
Simon <bardrabbit@gmail.com>
2021-02-01 00:51:40 -0500

    modify the updateText function so that the output letters are not one letter delay

--- script.js
+++ script.js
@@ -1,35 +1,5 @@
-const input = document.querySelector('#text-input');
-const output = document.querySelector('#text-output');
-const boldButton = document.querySelector('#bold');
-const italicButton = document.querySelector('#italic');
-const underlineButton = document.querySelector('#underline');
-const alignButtons = document.querySelectorAll('align btn');
-// const leftAlignButton = document.querySelector('#left-align');
-// const centerAlignButton = document.querySelector('#center-align');
-// const rightAlignButton = document.querySelector('#right-align');
-
-document.addEventListener('keypress', updateText);
-boldButton.addEventListener('click', makeBold);
-italicButton.addEventListener('click', makeItalic);
-underlineButton.addEventListener('click', makeUnderline);
-alignButtons.forEach((alignBtn) =>
-  alignBtn.addEventListener('click', alignText),
-);
-
 function updateText() {
-  output.innerText = input.value;
+  let inputText = document.querySelector('#text-input').value;
+  let outPut = document.querySelector('#text-output');
+  outPut.innerText = inputText;
 }
-
-function makeBold() {
-  output.classList.toggle('bold');
-}
-
-function makeItalic() {
-  output.classList.toggle('italic');
-}
-
-function makeUnderline() {
-  output.classList.toggle('underline');
-}
-
-function alignText(alignType) {}
